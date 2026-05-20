ee10565d17dbebfb7ceecc05160d86255cefccfb
rushil <rushilshah23@users.noreply.github.com>
2026-05-21 01:27:33 +0530

    replaced s3 with hyphen

--- modules/remote-infra/s3.tf
+++ modules/remote-infra/s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "tf-state-bucket-resource" {
-  bucket = "${var.enviornment}_${var.bucket_name}"
+  bucket = "${var.enviornment}-${var.bucket_name}"
 
   tags = {
-    Name = "${var.enviornment}_${var.bucket_name}"
+    Name = "${var.enviornment}-${var.bucket_name}"
   }
 }
