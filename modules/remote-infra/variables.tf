c8aae04e2816aa115978658b8324ce576b0e498d
rushil <rushilshah23@users.noreply.github.com>
2026-07-13 01:36:52 +0530

    Removed dynamodb and cleaned tf

--- modules/remote-infra/variables.tf
+++ modules/remote-infra/variables.tf
@@ -6,14 +6,8 @@ variable "bucket_name" {
   type = string
 }
 
-variable "dynamodb_table_name" {
-  type = string
-}
 
-variable "hash_key" {
-  type      = string
-  sensitive = true
-}
+
 variable "region" {
   type = string
 }
